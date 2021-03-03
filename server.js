ba063ad69365fd4e56e3cdd58f41ccd01f82a770
jlw429 <jlw00329@gmail.com>
2021-03-02 22:10:40 -0500

    more on the jscript

--- server.js
+++ server.js
@@ -12,26 +12,23 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log('connected as id' + connection.threadID);
+  console.log(`Welcome! Connected as id ${connection.threadId}`);
   inqStart();
 });
-
-
 
 function inqStart() {
   inquirer
     .prompt({
       type: 'list',
       choices: [
-        'Add Department',
-        'Add Role',
-        'Add Associate',
         'View Department',
         'View Role',
         'View Employees',
-        'View Employees by Manager',
-        'UPDATE Employee Role',
-        'UPDATE Manager Role',
+        'Add Department',
+        'Add Role',
+        'Add Associate',
+        'UPDATE Employee Role ID',
+        "UPDATE Employee's Manager Role ID",
         'DELETE Department',
         'DELETE Role',
         'DELETE Employee',
@@ -43,15 +40,6 @@ function inqStart() {
     .then((result) => {
       console.log('You inputted: ' + result.choiceInput);
       switch (result.choiceInput) {
-        case 'Add Department':
-          deptAdd();
-          break;
-        case 'Add Role':
-          roleAdd();
-          break;
-        case 'Add Associate':
-          associateAdd();
-          break;
         case 'View Department':
           deptView();
           break;
@@ -61,13 +49,19 @@ function inqStart() {
         case 'View Employees':
           employeeView();
           break;
-        case 'View Employees by Manager':
-          employeeManagerView();
+        case 'Add Department':
+          deptAdd();
           break;
-        case 'UPDATE Employee Role':
+        case 'Add Role':
+          roleAdd();
+          break;
+        case 'Add Associate':
+          associateAdd();
+          break;
+        case 'UPDATE Employee Role ID':
           empRoleUpdate();
           break;
-        case 'UPDATE Employees Manager':
+        case "UPDATE Employee's Manager Role ID":
           mgrRoleUpdate();
           break;
         case 'DELETE Department':
@@ -97,22 +91,27 @@ function deptAdd() {
         'Information Technology',
         'Human Resources',
         'Research and Development',
-        'Sales'
+        'Sales',
       ],
     })
     .then((answer) => {
       const query = 'INSERT INTO department (name) VALUES (?)';
       connection.query(query, [answer.deptName], (err, res) => {
         if (err) throw err;
-        console.table(res)
+        console.table(res);
         inqStart();
       });
-  });
-};
+    });
+}
 
 function roleAdd() {
   inquirer
     .prompt([
+      {
+        name: 'idDept',
+        type: 'input',
+        message: 'Enter the dept ID:',
+      },
       {
         name: 'roleTitle',
         type: 'input',
@@ -123,21 +122,20 @@ function roleAdd() {
         type: 'input',
         message: 'Enter salary amount: (annually)',
       },
-      {
-        name: 'idDept',
-        type: 'input',
-        message: 'Enter the dept ID:',
-      },
     ])
     .then((answer) => {
-      const query = "INSERT INTO role (id, title, salary) VALUES ( ?, '?', ? )";
-      connection.query(query, [answer.idDept], [answer.roleTitle], [answer.salary], (err, res) => {
+      const query = 'INSERT INTO role (id, title, salary) VALUES ( ?, ?, ? )';
+      connection.query(
+        query,
+        [answer.idDept, answer.roleTitle, answer.salary],
+        (err, res) => {
           if (err) throw err;
-          console.table(res);
+          console.table('successfully added.');
           inqStart();
-        })
+        }
+      );
     });
-};
+}
 
 function associateAdd() {
   inquirer
@@ -165,56 +163,45 @@ function associateAdd() {
     ])
     .then((answer) => {
       const query =
-        "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('?', '?', ?, ?);";
+        'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
       connection.query(
         query,
-        [answer.firstName],
-        [answer.lastName],
-        [answer.roleId],
-        [answer.mgrId], (err, res) => {
+        [answer.firstName, answer.lastName, answer.roleId, answer.mgrId],
+        (err, res) => {
           if (err) throw err;
-          console.table(res);
+          console.table('successfully added.');
           inqStart();
-        })
+        }
+      );
     });
-};
+}
 //VIEW
 function deptView() {
-    const query = "SELECT * FROM department";
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-        console.table(res);
-        inqStart();
-      })
-};
+  const query = 'SELECT * FROM department';
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    inqStart();
+  });
+}
 
 function roleView() {
-    const query = "SELECT * FROM role";
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-        console.table(res);
-        inqStart();
-      })
-};
+  const query = 'SELECT * FROM role';
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    inqStart();
+  });
+}
 
 function employeeView() {
-    const query = "SELECT * FROM employee";
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-        console.table(res);
-        inqStart();
-      })
-};
-
-function employeeManagerView() {
-    const query = "SELECT * FROM employee (first_name, last_name, manager_id) VALUES ('?', '?', ?)";
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-        console.table(res);
-        inqStart();
-      })
-};
-
+  const query = 'SELECT * FROM employee';
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    inqStart();
+  });
+}
 //UPDATE
 
 function empRoleUpdate() {
@@ -223,57 +210,94 @@ function empRoleUpdate() {
       {
         name: 'firstName',
         type: 'input',
-        message: "Please enter your first name:",
+        message: 'Please enter your first name:',
       },
       {
         name: 'lastName',
         type: 'input',
-        message: "Enter your last name:",
+        message: 'Enter your last name:',
       },
       {
         name: 'newRoleId',
         type: 'input',
-        message: "Enter your NEW role ID:",
-      }
+        message: 'Enter your NEW role ID:',
+      },
     ])
     .then((answer) => {
-      const query = "SELECT * FROM employee; UPDATE employee SET role_id = ? WHERE first_name = '?' AND last_name = '?'";
-      connection.query(
-        query, [answer.firstName], [answer.lastName], [answer.newRoleId], (err, res) => {
-          if (err) throw err;
-          console.table(res);
-          inqStart();
-        })
-  });
-};
+      const query = `UPDATE employee SET role_id = ${answer.newRoleId} WHERE first_name = "${answer.firstName}"  AND last_name = "${answer.lastName}"`;
+      connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table('Successfully Updated.');
+        inqStart();
+      });
+    });
+}
 
-// function mgrRoleUpdate() {
-//   inquirer
-//     .prompt([
-//       {
-//         name: 'firstName',
-//         type: 'input',
-//         message: "Please enter your first name:",
-//       },
-//       {
-//         name: 'lastName',
-//         type: 'input',
-//         message: "Enter your last name:",
-//       },
-//       {
-//         name: 'newRoleId',
-//         type: 'input',
-//         message: "Enter your NEW role ID:",
-//       }
-//     ])
-//     .then((answer) => {
-//       const query = "SELECT * FROM employee; UPDATE employee SET role_id = ? WHERE first_name = '?' AND last_name = '?'";
-//       connection.query(
-//         query, { firstName: answer.firstName }, { lastName: answer.lastName }, { newRoleId: answer.newRoleId }, (err, res => {
-//           if (err) throw err;
-//           console.table(res);
-//           inqStart();
-//         })
-//       );
-//   });
-// };
+function mgrRoleUpdate() {
+  inquirer
+    .prompt([
+      {
+        name: 'firstName',
+        type: 'input',
+        message: 'Please enter your first name:',
+      },
+      {
+        name: 'lastName',
+        type: 'input',
+        message: 'Enter your last name:',
+      },
+      {
+        name: 'newMgrRoleId',
+        type: 'input',
+        message: 'Enter your NEW manager ID:',
+      },
+    ])
+    .then((answer) => {
+      const query = `UPDATE employee SET manager_id = ${answer.newMgrRoleId} WHERE first_name = "${answer.firstName}"  AND last_name = "${answer.lastName}"`;
+      connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table('Successfully Updated.');
+        inqStart();
+      });
+    });
+}
+//DELETE
+function deptDelete() {
+  inquirer
+    .prompt([
+      {
+        name: 'deptToDelete',
+        type: 'input',
+        message: 'Please enter the department to terminate:',
+      },
+    ])
+    .then((answer) => {
+      const query = `DELETE FROM department WHERE name = "${answer.deptToDelete}"`;
+      connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table('Termination Successful.');
+        deptView();
+        inqStart();
+      });
+    });
+}
+
+function employeeDelete() {
+  inquirer
+    .prompt([
+      {
+        name: 'employeeToDelete',
+        type: 'input',
+        message: 'Please enter the associate to terminate:',
+      },
+    ])
+    .then((answer) => {
+      const query = `DELETE FROM employee WHERE name = "${answer.employeeToDelete}"`;
+      connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table('Termination Successful.');
+        deptView();
+        inqStart();
+      });
+    });
+}
